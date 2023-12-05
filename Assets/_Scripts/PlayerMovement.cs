0ddad45151f5bd2b25c9d1eab6218660655f5b4a
Fightbuster2 <Rune.cuijpers@sintlucasedu.nl>
2023-12-05 14:12:10 +0100

    player Movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public void speed;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * xInput * speed * Time.deltaTime);
+
+        float yInput = Input.GetAxis("Vertical");
+        transform.Translate(vector2.up * yInput * speed * Time.deltaTime);
     }
 }
