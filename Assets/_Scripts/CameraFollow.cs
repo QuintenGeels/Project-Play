bfece59bed158d4115347a3ec1a4c779dc52168e
Fightbuster2 <Rune.cuijpers@sintlucasedu.nl>
2023-12-05 14:33:10 +0100

    added camera follow

--- Assets/_Scripts/CameraFollow.cs
+++ Assets/_Scripts/CameraFollow.cs
@@ -1,16 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
-    public float smoothTime = 0.25f;
-    private Vector3 velocity = Vector3.zero;
+    public float FollowSpeed = 2f;
+    public Transform target;
 
-    [SerializeField] private Transform target;
-
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
