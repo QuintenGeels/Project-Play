e487fa5921b5c7620efa715142452c7b92025c76
QGeels <94874270+QGeels@users.noreply.github.com>
2023-12-06 10:47:54 +0100

    added player animations

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
+    public float yInput = 0f;
+    public float xInput = 0f;
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private enum MovementState { idle, walkingUp, walkingDown, walkingLeft, walkingRight}
+    private MovementState State = MovementState.idle;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,23 +32,49 @@ public class PlayerMovement : MonoBehaviour
     {
 
         Move();
+        UpdateAnimationUpdate();
     }
 
     void Move()
     {
-        float xInput = Input.GetAxis("Horizontal");
+        xInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * xInput * speed * Time.deltaTime);
-        if (xInput > 0)
+
+        yInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * yInput * speed * Time.deltaTime);
+
+
+    }
+    private void UpdateAnimationUpdate()
+    {
+        MovementState State;
+
+        if (yInput > 0f)
         {
-            Debug.Log("xInput");
+            State = MovementState.walkingUp;            
+        }
+        else if (yInput < 0f)
+        {
+            State = MovementState.walkingDown;            
+        }
+        else
+        {
+            State = MovementState.idle;
+        }
+        if (xInput > 0f)
+        {
+            State = MovementState.walkingLeft;
+        }
+        else if (xInput < 0f)
+        {
+            State = MovementState.walkingRight;
+        }
+        else
+        {
+            State = MovementState.idle;
         }
         
 
-        float yInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector2.up * yInput * speed * Time.deltaTime);
-        if (yInput > 0)
-        {
-            Debug.Log("yInput");
-        }
+        anim.SetInteger("State", (int)State);
     }
 }
